ad19d8d60c45b5425599621e4b00717b90685d47
Pierre Bride <pierre_bride@icloud.com>
2021-08-13 12:29:09 +0200

    create db seed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
   def index
     @lists = List.all
   end
 
   def show
     @list = List.find(params[:id])
+    @review = Review.new(list: @list)
   end
 
   def new
@@ -12,14 +14,27 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(lists_params)
-    @list.save
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list.destroy
     redirect_to lists_path
   end
 
   private
 
-  def lists_params
+   def set_list
+    @list = List.find(params[:id])
+  end
+
+
+  def list_params
     params.require(:list).permit(:name)
   end
 
